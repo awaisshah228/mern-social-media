0bd8a966d0f3479928358f3bd9d1e6b266bc9ab6
Muhammad Awais Shah <82607059+awaisaslam186@users.noreply.github.com>
2021-10-10 17:29:25 +0500

    Notify to Alert type changes

--- client/src/components/alert/Toasts.js
+++ client/src/components/alert/Toasts.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Toast,ToastContainer } from 'react-bootstrap'
 import  SingleToast from'./SingleToast'
 import { useSelector,useDispatch } from 'react-redux'
+import {GLOBALTYPES} from '../../redux/actions/globalType'
 const Toasts = () => {
     const alert = useSelector(state => state.alert)
     const  dispatch = useDispatch()
@@ -10,8 +11,8 @@ const Toasts = () => {
         <>
          
   <ToastContainer position="top-end" className="p-3" style={{zIndex : 50}}>
-      {alert.error && <SingleToast msg={alert.error} bg='danger' title='Error' handleshow={()=>{dispatch({type: 'NOTIFY',payload:{}})}}/>}
-      {alert.success && <SingleToast msg={alert.success} bg='success' title='Success'  handleshow={()=>{dispatch({type: 'NOTIFY',payload:{}})}}/>}
+      {alert.error && <SingleToast msg={alert.error} bg='danger' title='Error' handleshow={()=>{dispatch({type: GLOBALTYPES.ALERT,payload:{}})}}/>}
+      {alert.success && <SingleToast msg={alert.success} bg='success' title='Success'  handleshow={()=>{dispatch({type: GLOBALTYPES.ALERT,payload:{}})}}/>}
    {/* <SingleToast/> */}
   </ToastContainer>
 
